c6ec0957886fd5bf3bf016b968b82aa4e8aa6eaa
abdelouahed <doonabdo98@gmail.com>
2020-06-21 23:42:48 +0000

    add skills component to home components

--- src/components/homeComponents/Skills.vue
+++ src/components/homeComponents/Skills.vue
@@ -1,45 +1,56 @@
 <template>
-  <v-container class="skills">
-      <div class="display-3 my-10" ><p> My Skills </p></div>
-      <v-row>
-        <v-col col="4">
-            <img 
-            src="/images/fullstack.svg"
-            width="200"
-            height="150"
-            >
+  <div class="skills container">
+      <div class="display-3"><p> My Skills </p></div>
+      <div class="row">
+        <div class="col-md-4">
+            <g-image 
+            src="~/assets/images/fullstack.svg"
+            class="img"
+            />
             <span>
                 <h2>Full stack web developer</h2>
                 Full stack web developer with laravel framework
                 for backend and vuejs for frontend
                 for CMS i'm familiar with drupal and wordpress
             </span>
-        </v-col>
-        <v-col col="4">
-            <img 
-            src="/images/ui.svg"
-            width="200"
-            height="150"
-            >
+        </div>
+        <div class="col-md-4">
+            <g-image 
+            src="~/assets/images/ui.svg"
+            class="img"
+            />
             <span>
                 <h2>UI and UX</h2>
                 Build attratcive user intefaces and provide best user experience
                 using lattest and hottest technologies and frameworks
             </span>
-        </v-col>
-        <v-col col="4">
-            <img 
-            src="/images/mobileApps.svg"
-            width="200"
-            height="150"
-            >
+        </div>
+        <div class="col-md-4">
+            <g-image 
+            src="~/assets/images/mobileApps.svg"
+             class="img"
+            />
             <span>
                <h2>Mobile apps developer</h2>
                Mobile apps development with hight performance and a super 
                integration with your website using nativeScript that support vuejs
                for mobile apps development
             </span>
-        </v-col>
-      </v-row>
-  </v-container>   
+        </div>
+      </div>
+  </div>   
 </template>
+
+<style scoped>
+ .display-3 {
+     text-align: center;
+ }
+ .img {
+     width: 100%;
+     height: 150px;
+     margin-top: 20px;
+ }
+ .col-md-4 {
+     text-align: center;
+ }
+</style>
