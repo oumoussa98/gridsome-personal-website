735fa5f25cb6d9289454c8e835f58e9baf4579e1
abdelouahed <doonabdo98@gmail.com>
2020-06-27 21:22:10 +0000

    add some styling to about page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,7 +1,16 @@
 <template>
   <Layout>
-    <h1>About us</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    <div class="container about">
+      <div class="col-md-8">
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Error doloremque omnis animi, eligendi magni a voluptatum,
+          vitae, consequuntur rerum illum odit fugit assumenda rem
+          dolores inventore iste reprehenderit maxime! Iusto.
+        </p>
+      </div>
+    </div>
   </Layout>
 </template>
 
@@ -12,3 +21,10 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  .about {
+    min-height: 600px;
+    font-size: 20px;
+  }
+</style>
