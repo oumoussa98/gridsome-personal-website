40a205de8f05ea197172d269e076614f81d82be4
abdelouahed <doonabdo98@gmail.com>
2020-06-30 20:54:52 +0000

    add Post component for displaying posts content

---
+++ src/templates/Post.vue
@@ -0,0 +1,51 @@
+<template>
+<layout>
+  <div class="container markdown-body">
+      <h1>{{$page.post.title}} post</h1>
+      <img :src="$page.post.img" alt="Post image" width="100%" height="250" >
+      <div v-html="$page.post.content">
+      </div>
+
+  </div>
+</layout>
+</template>
+
+<page-query>
+
+    query Post($path: String) {
+        post: post (path: $path){
+        title
+        content
+        img
+    }
+}
+       
+</page-query>
+
+<script>
+export default {
+    metaInfo() {
+        return {
+            title: this.$page.post.title
+        }
+    }
+
+}
+</script>
+
+<style>
+@import url('https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/4.0.0/github-markdown.min.css');
+	.markdown-body {
+		box-sizing: border-box;
+		min-width: 200px;
+		max-width: 980px;
+		margin: 0 auto;
+		padding: 45px;
+	}
+
+	@media (max-width: 767px) {
+		.markdown-body {
+			padding: 15px;
+		}
+	}
+</style>
