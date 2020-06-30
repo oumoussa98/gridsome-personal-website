2a9afe2a2ab2681b7cca8a0b16651786e9fe73b4
abdelouahed <doonabdo98@gmail.com>
2020-06-30 20:52:02 +0000

    make projects section use ProjectCard component

--- src/components/homeComponents/whatIBuilt.vue
+++ src/components/homeComponents/whatIBuilt.vue
@@ -4,22 +4,12 @@
       <div class="row">
         <div v-for="(project,i) in projects" :key="i"  >
           <div class="col-md-4">
-            <div class="card">
-              <div class="card-title">
-                <h3>{{ project.title }}</h3>
-              </div>
-              <div class="card-image">
-                <img alt="Project screenshot" v-bind:src="project.img">
-              </div>
-              <div class="card-footer">
-                <p>
-                   {{ project.description }}
-                </p>
-              </div>
-              <div id="card-links">
-                <a :href="project.href" target="_blanck">See The Project</a>
-              </div>
-            </div>
+            <Card
+            :title="project.title"
+            :img="project.img"
+            :description="project.description"
+            :href="project.href"
+            />
           </div>
         </div>
       </div>
@@ -27,9 +17,14 @@
 </template>
 
 <script defer>
+
+import Card from '~/components/ProjectCard' ;
+
 export default {
-
-
+// Components 
+  components: {
+    Card
+  },
 // Data ==========================
   data:() => ({
 
@@ -61,6 +56,7 @@ export default {
           },
       ],
   }),
+
 }
 </script>
 
@@ -68,38 +64,6 @@ export default {
 
 .display-3 {
   text-align: center;
-}
-
-.card {
-  width: 100%;
-  height: 420px;
-  text-align: center;
-  border-radius: 6px;
-  margin: 30px 0;
-  box-shadow: 0px 1px 12px rgba(0, 0, 0, 0.3);
-}
-.card-title {
-  min-height: 50px ;
-  padding: 10px;
-}
-.card-image img {
-  width: 100%;
-  height: 200px;
-}
-#card-links  {
-  float: left ;
-  margin: 10px 10px 0px 20px;  
-}
-#card-links a  {
-  padding: 8px;
-  color: rgb(7, 165, 170);
-  cursor: pointer;
-  text-decoration: none;
-  border-radius:  6px;
-}
-#card-links a:hover {
-    background-color: rgba(170, 164, 164, 0.171);
-    transition: all 0.4s;
 }
   
 </style>
