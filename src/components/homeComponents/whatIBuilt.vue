3ce09c9d50a43bd8a7b4d6a2c267c5adba1f009e
abdelouahed <doonabdo98@gmail.com>
2020-07-06 21:47:13 +0000

    move cards to cards folder in components folder

--- src/components/homeComponents/whatIBuilt.vue
+++ src/components/homeComponents/whatIBuilt.vue
@@ -1,91 +1,91 @@
 <template>
   <div class="skills container" id="my-projects">
-      <div class="display-3" ><p> What I Built </p></div>
-      <div class="row">
-        <div v-for="(project,i) in projects" :key="i"  >
-          <div class="col-md-4">
-            <div :class="{animate: animate}">
-              <Card
-            :title="project.title"
-            :img="project.img"
-            :description="project.description"
-            :href="project.href"
+    <div class="display-3">
+      <p>What I Built</p>
+    </div>
+    <div class="row">
+      <div v-for="(project,i) in projects" :key="i">
+        <div class="col-md-4">
+          <div :class="{animate: animate}">
+            <Card
+              :title="project.title"
+              :img="project.img"
+              :description="project.description"
+              :href="project.href"
             />
-            </div>
           </div>
         </div>
       </div>
-  </div>   
+    </div>
+  </div>
 </template>
 
 <script defer>
-
-import Card from '~/components/ProjectCard' ;
+import Card from "~/components/cards/ProjectCard";
 
 export default {
-// Components 
+  // Components
   components: {
     Card
   },
-// Data ==========================
-  data:() => ({
-        animate: false ,
+  // Data ==========================
+  data: () => ({
+    animate: false,
 
-        projects: [
-          {
-            title: 'Tool for management ',
-            description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit
+    projects: [
+      {
+        title: "Tool for management ",
+        description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit
                          ipsa quos officiis dolorum, unde eius beatae cumque iusto maxime `,
-            href: 'https://github.com/oumoussa98/tool-for-management',
-            img: '/uploads/screenshot1.png'
-          },
-          {
-            title: 'Blog with php',
-            description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit 
+        href: "https://github.com/oumoussa98/tool-for-management",
+        img: "/uploads/screenshot1.png"
+      },
+      {
+        title: "Blog with php",
+        description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit 
                           ipsa quos officiis dolorum, unde eius beatae cumque iusto maxime `,
-            href: 'https://github.com/oumoussa98/blog-with-php',
-            img: 'uploads/screenshot2.png'
-          },
-          
-          {
-            title: 'Ourika blog',
-            description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit
+        href: "https://github.com/oumoussa98/blog-with-php",
+        img: "uploads/screenshot2.png"
+      },
+
+      {
+        title: "Ourika blog",
+        description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit
                           ipsa quos officiis dolorum, unde eius beatae cumque iusto maxime `,
-            href: 'https://github.com/oumoussa98/Blog-laravel',
-            img: "uploads/screenshot3.png"   
-          },
-        {
-            title: 'Android app',
-            description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit
+        href: "https://github.com/oumoussa98/Blog-laravel",
+        img: "uploads/screenshot3.png"
+      },
+      {
+        title: "Android app",
+        description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit
                           ipsa quos officiis dolorum, unde eius beatae cumque iusto maxime `,
-            href: 'https://something.com',
-            img: 'https://i.pcmag.com/imagery/articles/06iVYK5P05qeiONvmtOi7s6-11.fit_scale.size_2698x1517.v1569483964.jpg'
-          },
-      ],
+        href: "https://something.com",
+        img:
+          "https://i.pcmag.com/imagery/articles/06iVYK5P05qeiONvmtOi7s6-11.fit_scale.size_2698x1517.v1569483964.jpg"
+      }
+    ]
   }),
 
-    methods: {
-        handleScroll() {
-          let y = scrollY ;
-          if( y >= 980 ) {
-              this.animate = true ;
-          }
-        }
-    },
-    
-    mounted() {
-        window.addEventListener('scroll', this.handleScroll )
-    },
+  methods: {
+    handleScroll() {
+      let y = scrollY;
+      if (y >= 980) {
+        this.animate = true;
+      }
+    }
+  },
 
-}
+  mounted() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+};
 </script>
 
 <style scoped>
-
 .display-3 {
   text-align: center;
 }
 .animate {
-    animation: scaleDiv 2s linear;
+  animation: scaleDiv 1.5s linear;
 }
 </style>
