b15b060f24106cc3345fa5cf1b9a08da9c5b2290
abdelouahed <doonabdo98@gmail.com>
2020-07-26 11:08:56 +0000

    update dark and light component

--- src/components/DarkLight.vue
+++ src/components/DarkLight.vue
@@ -1,85 +1,85 @@
 <template>
-  <div class="other">
-    <button v-on:click="darkModeToggle">
-      <i :class="fa"></i>
-    </button>
-  </div>
+    <div class="other">
+        <button v-on:click="darkModeToggle">
+            <i :class="fa"></i>
+        </button>
+    </div>
 </template>
 
 <script>
 export default {
-  // Data =================================
-  data: () => ({
-    // light and dark mode
-    fa: "fas fa-moon",
-    darkMode: null
-  }),
-  // Methods =====================================
-  methods: {
-    enableDarkMode() {
-      // 1. Add the class to the body
-      document.body.classList.add("darkmode");
-      // 2. Update darkMode in localStorage
-      localStorage.setItem("darkMode", "enabled");
-      this.fa = "fas fa-sun";
-    },
+    // Data =================================
+    data: () => ({
+        // light and dark mode
+        fa: "fas fa-moon",
+        darkMode: null,
+    }),
+    // Methods =====================================
+    methods: {
+        enableDarkMode() {
+            // 1. Add the class to the body
+            document.body.classList.add("darkmode");
+            // 2. Update darkMode in localStorage
+            localStorage.setItem("darkMode", "enabled");
+            this.fa = "fas fa-sun";
+        },
 
-    disableDarkMode() {
-      // 1. Remove the class from the body
-      document.body.classList.remove("darkmode");
-      // 2. Update darkMode in localStorage
-      localStorage.setItem("darkMode", null);
-      this.fa = "fas fa-moon";
-    },
+        disableDarkMode() {
+            // 1. Remove the class from the body
+            document.body.classList.remove("darkmode");
+            // 2. Update darkMode in localStorage
+            localStorage.setItem("darkMode", null);
+            this.fa = "fas fa-moon";
+        },
 
-    // toggle between dark and light mode
-    darkModeToggle() {
-      // get their darkMode setting
-      this.darkMode = localStorage.getItem("darkMode");
-      // if it not current enabled, enable it
-      if (this.darkMode !== "enabled") {
-        this.enableDarkMode();
-        // if it has been enabled, turn it off
-      } else {
-        this.disableDarkMode();
-      }
-    }
-  },
-  // vueJs mounted function
-  mounted() {
-    // check for saved 'darkMode' in localStorage
-    this.darkMode = localStorage.getItem("darkMode");
-    // If the user already visited and enabled darkMode
-    // start things off with it on
-    if (this.darkMode === "enabled") {
-      this.enableDarkMode();
-    }
-  }
+        // toggle between dark and light mode
+        darkModeToggle() {
+            // get their darkMode setting
+            this.darkMode = localStorage.getItem("darkMode");
+            // if it not current enabled, enable it
+            if (this.darkMode !== "enabled") {
+                this.enableDarkMode();
+                // if it has been enabled, turn it off
+            } else {
+                this.disableDarkMode();
+            }
+        },
+    },
+    // vueJs mounted function
+    beforeMount() {
+        // check for saved 'darkMode' in localStorage
+        this.darkMode = localStorage.getItem("darkMode");
+        // If the user already visited and enabled darkMode
+        // start things off with it on
+        if (this.darkMode === "enabled") {
+            this.enableDarkMode();
+        }
+    },
 };
 </script>
 
 <style scoped>
 .other {
-  width: 10%;
-  float: right;
-  margin: 15px 20px 0 0;
+    width: 10%;
+    float: right;
+    margin: 15px 20px 0 0;
 }
 .other button {
-  float: right;
+    float: right;
 }
 .other button i {
-  font-size: 25px;
-  float: right;
+    font-size: 25px;
+    float: right;
 }
 @media only screen and (max-width: 800px) {
-  .other {
-    width: 10%;
-    text-align: center;
-  }
-  .other button i {
-    font-size: 25px;
-    margin: 0 20px 10px 0;
-    text-align: center;
-  }
+    .other {
+        width: 10%;
+        text-align: center;
+    }
+    .other button i {
+        font-size: 25px;
+        margin: 0 20px 10px 0;
+        text-align: center;
+    }
 }
 </style>
